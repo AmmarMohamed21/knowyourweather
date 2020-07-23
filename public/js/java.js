9ed890de30a4e9254dddf42894ab7f06ee46ba8a
Ammar Mohamed Sobhi <ammarmohamed13@gmail.com>
2020-07-23 23:43:39 +0200

    Update java.js

--- public/js/java.js
+++ public/js/java.js
@@ -4,9 +4,11 @@ function printanswer(cityname)
  fetch('https://api.openweathermap.org/data/2.5/weather?q='+cityname.value+'&appid=e30813cf68d3ddcdedc614b02b5337d1&units=metric')
   .then(response => response.json())
   .then(data => {
-    document.getElementById("result").innerHTML = data.main.temp;
-    document.getElementById("result").innerHTML = document.getElementById("result").innerHTML + "<br>"+data.weather[0].description;
-
+    document.getElementById("tempnow").innerHTML = "Temperature Now: "  + data.main.temp + "°C";
+    document.getElementById("tempmax").innerHTML = "Max Temperature: " + data.main.temp_max + "°C";
+    document.getElementById("tempmin").innerHTML = "Min Temperature: " + data.main.temp_min + "°C";
+    document.getElementById("hum").innerHTML = "Humidity: " + data.main.humidity +"%";
+    document.getElementById("desc").innerHTML =  "Describtion: " +data.weather[0].description;
   } );
 
 
